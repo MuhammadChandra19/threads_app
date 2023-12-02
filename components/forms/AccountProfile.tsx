90657f8eadf01c35a3f1dfe85f83b8e668e25938
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2023-12-03 00:18:02 +0700

    create upload handler

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -16,7 +16,7 @@ import { zodResolver }  from '@hookform/resolvers/zod'
 import { UserValidationSchema } from '@/lib/validations/user';
 import * as z from 'zod'
 import Image from 'next/image';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { Textarea } from '@/components/ui/textarea';
 
 
@@ -26,6 +26,7 @@ interface Props {
     id: string;
     objectId: string;
     username: string;
+    name: string;
     bio: string;
     image: string;
   }
@@ -33,19 +34,39 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle}: Props) => {
+  const [files, setFiles] = useState<File[]>([])
+
   const form = useForm({
     resolver: zodResolver(UserValidationSchema),
     defaultValues: {
-      profile_photo: '',
-      name: '',
-      username: '',
-      bio: ''
+      profile_photo: user?.image || '',
+      name:  user?.name || '',
+      username:  user?.username || '',
+      bio:  user?.bio || '',
     }
   })
 
 
-  const handleImage = (e: ChangeEvent, fieldChanged: (v: string) => void) => {
+  const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChanged: (v: string) => void) => {
     e.preventDefault()
+
+    const fileReader = new FileReader()
+
+    if(e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0]
+
+      setFiles(Array.from(e.target.files))
+
+      if(!file.type.includes('image')) return
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || ''
+
+        fieldChanged(imageDataUrl)
+      }
+
+      fileReader.readAsDataURL(file)
+    }
   }
 
   function onSubmit(values: z.infer<typeof UserValidationSchema>) {
