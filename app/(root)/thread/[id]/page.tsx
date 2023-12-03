12f99f14fa338f0703b60c34a0a27de2f869ef22
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2023-12-03 11:50:01 +0700

    enable comment

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,65 @@
+import ThreadCard from '@/components/cards/ThreadCard'
+import Comment from '@/components/forms/Comment'
+import { fetchThreadById } from '@/lib/actions/thread.action'
+import { fetchUser } from '@/lib/actions/user.action'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+
+const Page = async ({ params }: { params: { id: string }}) => {
+  if(!params.id) return null
+
+  const user = await currentUser()
+  if(!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if(!userInfo?.onboarded) redirect('/onboarding')
+
+
+  const thread = await fetchThreadById(params.id)
+  console.log(thread)
+
+  return (
+    <section className="relative">
+      <ThreadCard
+        key={thread._id.toString()}
+        id={thread._id.toString()}
+        currentUserId={user?.id || ''}
+        parentId={thread.parentId}
+        content={thread.text}
+        author={thread.author}
+        community={thread.community}
+        createdAt={thread.createdAt}
+        comments={thread.children}
+      />
+      <div className="mt-7">
+        <Comment 
+          threadId={thread.id}
+          currentUserImg={userInfo.image}
+          currentUserId={userInfo._id.toString()}
+        
+        />
+      </div>
+      <div className="mt-10">
+        {
+          thread.children.map((child: any) => (
+            <ThreadCard
+              key={child._id.toString()}
+              id={child._id.toString()}
+              currentUserId={child?.id || ''}
+              parentId={child.parentId}
+              content={child.text}
+              author={child.author}
+              community={child.community}
+              createdAt={child.createdAt}
+              comments={child.children}
+              isComment
+            />
+          ))
+        }
+      </div>
+    </section>
+  )
+}
+
+export default Page
